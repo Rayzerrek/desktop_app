6a71a689c6a082ccce63093f068ea9636a0dc889
Rayzerrek <rayserrek@gmail.com>
2025-10-11 22:30:45 +0200

    File structure

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,14 @@
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn add_numbers(a:i32, b:i32) -> i32 {
+    a + b
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![add_numbers])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
